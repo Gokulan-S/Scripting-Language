d45b223a7c4e7b841c040e2087d3da099a52a103
Gokulan-S <gokulan383@gmail.com>
2024-11-20 10:51:45 +0530

    Create PalindromeNumber.js

---
+++ Activity(Leetcode)/PalindromeNumber.js
@@ -0,0 +1,10 @@
+function isPalindrome(x) {
+    if (x < 0) return false;
+    const str = x.toString();
+    return str === str.split('').reverse().join('');
+}
+
+
+console.log(isPalindrome(121));  
+console.log(isPalindrome(-121)); 
+console.log(isPalindrome(10));   
